1ee0ac203daaeefcbe0383911fec6eefd9662bda
GnaphronG <gnaphron@gmail.com>
2019-01-03 19:00:18 -0800

    alert when a new client join the gang

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const io = require('socket.io')();
+const clients = [];
 
 io.on('connection', (client) => {
     client.on('subscribeToTimer', (interval) => {
-        console.log('client is subscribing to timer with interval ', interval);
-    setInterval(() => {
-        client.emit('timer', new Date());
-      }, interval);
+        console.log(`client #${client.id} is subscribing to timer with interval ${interval}`);
+        clients.forEach(c => {
+            c.emit('timer', `A new client joined the team`);
+        })
+        clients.push(client);
+        if (clients.length >= 5){
+            io.close();
+        }
+/*        setInterval(() => {
+            client.emit('timer', `Hello ${client.id} it is ${new Date()}`);
+        }, interval);*/
   });
 });
 
